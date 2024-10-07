a158a398df6348c3f1f779bfa380abb7681d9fe1
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-10-06 22:54:21 -0500

    fixed profile card for mobile devices

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,9 +4,9 @@ import {AiFillLinkedin, AiFillGithub} from "react-icons/ai";
 
 const Profile = () => {
   return (
-    <div className='relative h-screen max-w-full'>
+    <div className='relative h-auto min-h-screen md:h-screen max-w-full'>
     {/* Background with clouds */}
-    <div id="background-wrap" className='absolute top-0 left-0 w-full h-full overflow-hidden bg-gradient-to-b from-white to-sky-300'>
+    <div id="background-wrap" className='absolute top-0 left-0 w-full min-h-full overflow-hidden bg-gradient-to-b from-white to-sky-300'>
       <div className='cloud1'>
         <div className='cloud'></div>
       </div>
@@ -24,27 +24,39 @@ const Profile = () => {
       </div>
     </div>
 
-    {/* Profile banner */}
-    <div className="relative top-20 z-10 bg-gradient-to-b bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-sky-300 text-black flex flex-col items-center justify-center p-6 md:flex-row md:items-start md:p-20 shadow-2xl rounded-xl my-20 mx-10 py-20 rubic-regular">
+    {/* Profile Card */}
+    <div className="relative lg:top-16 z-10 bg-gradient-to-b bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-30 border border-sky-300 text-black flex flex-col items-center justify-center p-6 md:flex-row md:items-start md:p-20 shadow-2xl rounded-xl md:my-20 mx-4 md:mx-10 py-10   md:py-20 rubic-regular">
       
-      <div className="relative border-4 border-black rounded-full w-40 h-40 md:w-80 md:h-80 overflow-hidden">
+      <div className="relative rounded-full w-32 h-32 md:w-80 md:h-80 overflow-hidden">
         <Image src="/profile-square.jpg" alt="profile picture" className='object-cover' fill />
       </div>
 
       <div className="mt-10 md:mt-0 md:ml-10 text-center md:text-left flex flex-col items-center md:items-start">
-        <h2 className="text-3xl py-2 text-blackfont-medium md:text-5xl font-semibold">
+        <h2 className="text-3xl md:py-2 text-blackfont-medium md:text-5xl font-semibold">
           Carlos Guzman
         </h2>
         <h3 className="text-xl py-2 md:text-2xl">Software Developer</h3>
 
         <p className="text-md py-5 leading-8 text-black md:text-lg max-w-xl">
-          Freelancer providing services for programming and business content needs.
+          I am passionate about technoloy and nature but my true love is the piano 🎹
+          My technical strenghts are <strong>ASP.Net core</strong> and <strong>C# web development</strong>, although I have broad skills in different areas like <strong>Unity 3D</strong> game development and <strong>Communcations</strong>. 
+
         </p>
 
-        <div className="text-3xl flex gap-8 py-3">
-          <a href="https://www.linkedin.com/in/carlosd-guzman/" target="_blank" rel="noopener noreferrer" className='hover:scale-125'><AiFillLinkedin /></a>
-          <a href="https://github.com/Cdguzmanr" target="_blank" rel="noopener noreferrer" className='hover:scale-125'><AiFillGithub /></a>
+        <div className="text-3xl flex gap-8 py-3 items-center">
+          <a href="https://www.linkedin.com/in/carlosd-guzman/" target="_blank" rel="noopener noreferrer" className='hover:scale-125'>
+            <AiFillLinkedin />
+          </a>
+          <a href="https://github.com/Cdguzmanr" target="_blank" rel="noopener noreferrer" className='hover:scale-125'>
+            <AiFillGithub />
+          </a>
+          
+          <a href="#" className="bg-[#2D2928] text-lg text-white py-2 px-4 rounded hover:bg-[#29527A] hover:scale-105">
+            Download Resume
+          </a>
         </div>
+
+        
       </div>
     </div>
   </div>

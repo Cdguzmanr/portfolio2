fb6acf00019280ab5a74d48fa425f614ddee6207
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-10-09 21:29:23 -0500

    Added link preview metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
         <link rel="manifest" href="/site.webmanifest"/>
+
+        {/* Link preview metadata */}
+        <meta property="og:title" content="Carlos Guzman | Portfolio" />
+        <meta property="og:description" content="Check out my latest projects, skills, and professional experience. Let's connect!" />
+        <meta property="og:image" content="/profile-square.jpg" />
+        <meta property="og:url" content="https://carlosguzmanr.com/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
       </head>
 
       {/* Website content from page.tsx */}
